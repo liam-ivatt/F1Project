a4416f3dd3cec4ae9cfb173f4e8fea0450758b17
liam-ivatt <liamivatt@outlook.com>
2025-01-27 10:17:07 +0000

    Updated 2024 season to finished dataset, added plot function for driver placements

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 
-#drivers = pd.DataFrame([],[])
+def driver_list(data):
+    drivers = data.get("Driver").drop_duplicates()
+    print(drivers.to_string(index = False))
+    print("Driver count: ", drivers.count())
 
-F12024 = pd.DataFrame(pd.read_csv('2024.csv'))
+def average_placement(name, data):
 
-drivers = F12024.get("Driver",).drop_duplicates()
+    if name not in data["Driver"].values:
+        print("Average Placement Error: Driver not found")
+    else:
+        driver = data[data["Driver"].str.contains(name)]
+        driver = pd.to_numeric(driver["Position"], errors='coerce').mean().round()
+        print("Average driver placement is:", driver)
 
+    prompt = input("Average Placement Plot? (y/n): ")
+
+    if prompt == "y":
+
+        driver = data[data["Driver"].str.contains(name)]
+        trackpos = driver[["Track", "Position"]]
+
+        plt.xlabel("Track Position")
+        plt.ylabel("Race Placement")
+        plt.title("Driver's Positions on Different Tracks", fontsize=14)
+        fig, ax = plt.subplots()
+        plt.xticks(rotation=45, ha="right")
+        ax.plot(trackpos["Track"], trackpos["Position"].sort_values())
+        plt.show()
+
+
+def main():
+    data = pd.DataFrame(pd.read_csv('2024.csv'))
+    driver_list(data)
+    average_placement("Max Verstappen", data)
+
+if __name__ == "__main__":
+    main()
